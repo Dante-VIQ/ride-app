6b1818c7b98cafc134586cf3d10e038803ee359c
Dante-VIQ <damalide20@gmail.com>
2025-07-23 07:51:36 +0300

    alpine fix update

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -5,10 +5,10 @@
             sm:w-1/2" />
 
     <!-- Text overlay: bottom center, responsive width -->
-    <p class="absolute bottom-2 left-1/2 transform -translate-x-1/2
+    {{-- <p class="absolute bottom-2 left-1/2 transform -translate-x-1/2
               px-4 py-2 bg-gray-200 bg-opacity-90 text-gray-800 text-sm sm:text-base md:text-lg italic font-bold
               rounded shadow border border-gray-400 text-center z-20
               w-[90%] sm:w-[80%] md:w-[60%]">
         Compassionate, reliable and safe transportation every mile of the way
-    </p>
+    </p> --}}
 </section>
