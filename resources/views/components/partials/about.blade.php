9add1aa946e6d2daec79a1c3058187b6f2a144f4
Dante-VIQ <damalide20@gmail.com>
2025-07-31 17:03:25 +0300

    about page

--- resources/views/components/partials/about.blade.php
+++ resources/views/components/partials/about.blade.php
@@ -1,36 +1,31 @@
-@props(['about']) <!-- Changed from $about to $abouts since you're looping -->
-
 <div class="container-xxl py-5">
-    <div class="container mx-auto py-16">
+    <div class="container">
+        @if ($about)
+            <!-- Top Section: Images + Short Intro -->
+            <div class="row g-5 align-items-start">
+                <!-- Images -->
+                <div class="col-lg-5 col-md-12 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="d-flex flex-column">
+                        <img class="img-fluid rounded w-75 align-self-end mb-3"
+                             src="{{ $about->image ? asset('storage/' . $about->image) : asset('images/senior transport.webp') }}"
+                             alt="Senior Transport">
 
-    @if ($about)
-        <!-- Top Row: Images + Short Intro -->
-        <div class="grid lg:grid-cols-2 gap-10 items-start">
-            <!-- Images -->
-            <div class="flex flex-col items-end space-y-4">
-                <img class="rounded-lg w-3/4 shadow-md"
-                     src="{{ $about->image ? asset('storage/' . $about->image) : asset('images/senior transport.jpg') }}"
-                     alt="Senior Transport">
+                        <img class="img-fluid rounded w-50 bg-white pt-3 pe-3"
+                             src="{{ $about->photo ? asset('storage/' . $about->photo) : asset('images/medical van.webp') }}"
+                             alt="Medical Van" style="margin-top: -25%;">
+                    </div>
+                </div>
 
-                <img class="rounded-lg w-1/2 shadow-md -mt-12 bg-white p-2"
-                     src="{{ $about->photo ? asset('storage/' . $about->photo) : asset('images/medical van.webp') }}"
-                     alt="Medical Van">
+                <!-- Short Intro (Optional) -->
+                <div class="col-lg-7 col-md-12 wow fadeIn" data-wow-delay="0.3s">
+                    <p class="d-inline-block text-lg border rounded-pill py-2 px-4 mb-3">
+                        More About Us
+                    </p>
+                    <p class="text-gray-600 text-lg leading-relaxed">
+                        {{ Str::limit(strip_tags($about->description), 200, '...') }}
+                    </p>
+                </div>
             </div>
-
-            <!-- Short Intro -->
-            <div>
-                <p class="inline-block text-lg border border-green-600 text-green-800 rounded-full py-2 px-5 mb-3">
-                    More About Us
-                </p>
-                <p class="text-gray-700 text-lg leading-relaxed">
-                    {{ Str::limit(strip_tags($about->description), 200, '...') }}
-                </p>
-            </div>
-        </div>
-
-       
-    @endif
-
-</div>
-
+        @endif
+    </div>
 </div>
