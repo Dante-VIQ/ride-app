77f9fc573fa633cc3b7c8f2db6162f79a5b0c7da
Dante-VIQ <damalide20@gmail.com>
2025-07-31 16:51:30 +0300

    styling fix and multiple alpine souces fix

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -1,14 +1,17 @@
 <section class="relative image-stack bg-cover bg-center">
-    <!-- Foreground image: top-right, half width & height on large screens, full width on mobile -->
-    <img src="{{ asset('images/ride-aide.png') }}" alt="Ride Aide LLC"
-     class="absolute top-0 left-0 w-1/2 object-contain z-10
+    <div>
+        <!-- Foreground image: top-right, half width & height on large screens, full width on mobile -->
+        <img src="{{ asset('images/ride-aide.png') }}" alt="Ride Aide LLC"
+            class="absolute top-0 left-0 w-1/2 object-contain z-10
             sm:w-1/2" />
-
+    </div>
     <!-- Text overlay: bottom center, responsive width -->
-    <p class="absolute bottom-2 left-1/2 transform -translate-x-1/2
+    <p
+        class="absolute bottom-2 left-1/2 transform -translate-x-1/2
               px-4 py-2 bg-gray-200 bg-opacity-90 text-gray-800 text-sm sm:text-base md:text-lg italic font-bold
               rounded shadow border border-gray-400 text-center z-20
               w-[90%] sm:w-[80%] md:w-[60%]">
         Compassionate, reliable and safe transportation every mile of the way
     </p>
+
 </section>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,10 +43,11 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+        rel="stylesheet" />
 
     <link href="lib/animate/animate.min.css" rel="stylesheet">
-     <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
     <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
@@ -86,10 +87,14 @@
     @livewireScripts
 
     <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-        <script src="lib/wow/wow.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-3fp9o8l6EybC3xMFXLJH0iIEexzA62Q9r0iKh8wFv6A=" crossorigin="anonymous" defer></script>
+
+    {{-- Bootstrap 5 Bundle (with Popper) --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoYpVwT0nU6xI1p5Kh5W/S2nUd1p4n5fYh+7mIVo5NhcQ8+" crossorigin="anonymous" defer>
+    </script>
+    <script src="lib/wow/wow.min.js"></script>
     <script src="lib/easing/easing.min.js"></script>
     <script src="lib/waypoints/waypoints.min.js"></script>
     <script src="lib/counterup/counterup.min.js"></script>
