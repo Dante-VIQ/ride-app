851eeee6d0899350c56ede4eb66e93097a981188
Dante-VIQ <damalide20@gmail.com>
2025-12-27 12:10:38 +0300

    logo image

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<p {{ $attributes->merge(['class' => 'text-gray-700 font-semibold']) }}>RIDE AIDE</p>
+<p {{ $attributes->merge(['class' => 'text-blue-700 ']) }}><b class="text-blue-500 ">RIDE</b> <span class="text-green-600 font-semibold">AIDE</span></p>
